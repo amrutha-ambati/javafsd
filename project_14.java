0dc345212c72ae60af55d2f270ac515274435ccb
amrutha-ambati <109332265+amrutha-ambati@users.noreply.github.com>
2022-07-22 14:34:52 +0530

    Add files via upload

---
+++ project_14.java
@@ -0,0 +1,22 @@
+package project1;
+
+public class project_14 {
+
+    public static void main(String args[]) 
+    {
+        int[] array = new int[3];
+        try 
+        {
+            array[7] = 3;
+        }
+        catch (ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("Array index is out of bounds!"); 
+        }
+        finally 
+        {
+            System.out.println("The array is of size " + array.length);
+        }
+    }
+}
+
