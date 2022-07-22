308ddb2e6da39955cc7dd218c59be23b8a3d632e
amrutha-ambati <109332265+amrutha-ambati@users.noreply.github.com>
2022-07-22 13:28:40 +0530

    Add files via upload

---
+++ project4.java
@@ -0,0 +1,50 @@
+package simple;
+
+public class project4 {
+
+	int empId;
+	String empName;
+	String department;
+	float salary;
+	
+	//default constructor
+	public project4() {
+		empId=1;
+		empName="Emp001";
+		department="finance";
+		salary=35000;
+	}
+	
+	//parametrized constructor
+	public project4(int empId,String empName,String department,float salary) {
+		this.empId=empId;
+		this.empName=empName;
+		this.department=department;
+		this.salary=salary;
+	}
+	
+	public void display() {
+		System.out.println("Id: "+empId);
+		System.out.println("Name: "+empName);
+		System.out.println("Department: "+department);
+		System.out.println("Salary: "+salary);
+		System.out.println();
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		project4 e= new project4();
+		project4 e1= new project4(221, " Amrutha", "Training", 400000); 
+
+		//calling default constructor
+		e.display();
+		//parametrized constructor
+		e1.display();
+		
+		 
+	
+	}
+
+	
+}
